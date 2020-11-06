1bf9aa7e7d882f364d7f08e3727b82a18f284d72
jose-padin <jose.mendez@kubicum.com>
2020-11-06 23:55:40 +0100

    Support for add products to a shopping cart
    * Created cart and cartitem classes
    * Updated urls

--- project/core/company/urls/root.py
+++ project/core/company/urls/root.py
@@ -4,6 +4,6 @@ from ..views.company import company_list_view, company_detail_view
 
 urlpatterns = [
     re_path(r'^$', company_list_view, name='list'),
-    re_path(r'^company/(?P<id>\d+)$', company_detail_view, name='detail'),
-    # path('company/<int:id>/', company_list_view)
+    re_path(r'^company/(?P<company_id>\d+)$', company_detail_view, name='detail'),
+    re_path(r'^company/(?P<company_id>\d+)/cart/', include(('core.cart.urls', 'cart'))),
 ]
